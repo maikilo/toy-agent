8df18929a24cad12778b2cdf7e8b8040b727a8e8
maikilo <maija.loyska@gmail.com>
2025-08-10 18:33:48 +0300

    update tests

--- tests.py
+++ tests.py
@@ -1,32 +1,16 @@
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
+from functions.write_file_content import write_file
 
 
-def get_files_info_test():
-    print("Result for current directory:")
-    print(get_files_info("calculator", "."))
-
-    print("\nResult for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
-
-    print("\nResult for current directory:")
-    print(get_files_info("calculator", "/bin"))
-
-    print("\nResult for 'pkg' directory:")
-    print(get_files_info("calculator", "../"))
-
-
-def get_file_content_test():
-    result = get_file_content("calculator", "main.py")
+def test():
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
 
-    result = get_file_content("calculator", "pkg/calculator.py")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_file_content("calculator", "/bin/cat")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
 
 if __name__ == "__main__":
-    get_files_info_test()
-    get_file_content_test()
+    test()
