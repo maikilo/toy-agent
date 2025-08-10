035002c7f361eb52d6189198fe9f1fd78e100336
maikilo <maija.loyska@gmail.com>
2025-08-10 18:13:23 +0300

    edit get_files_info.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,25 +2,22 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(target_dir):
+        return f'Error: "{directory}" is not a directory'
     try:
-        result = ""
-        full_path = os.path.join(working_directory, directory)
-        abs_path = os.path.abspath(full_path)
-
-        if not os.path.isdir(abs_path):
-            result += f"\tError: '{directory}' is not a directory"
-            return result
-        
-        if not abs_path.startswith(os.path.abspath(working_directory)):
-            result += f"\tError: Cannot list '{directory}' as it is outside the permitted working directory"
-            return result
-        
-        for i, file in enumerate(sorted(os.listdir(abs_path))):
-            file_path = os.path.join(abs_path, file)
-            file_info = f"\n - {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}"
-            result += file_info
-        
-        return result
-
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error listing files: {e}"
